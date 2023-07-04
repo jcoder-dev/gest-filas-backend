46b5a0fd0fb93c7360cec69b97eaea4d51412d43
jcoder-dev <jorgeadao625@gmail.com>
2023-07-04 17:23:19 +0100

    add: Controlller para adicionar funcionário método adicionar funcionando

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -2,18 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FuncionarioRequest;
+use App\Models\Funcionario;
 use Illuminate\Http\Request;
+
+
 
 class FuncionarioController extends Controller
 {
+
+
     public function index()
     {
-        return view('usuarios.telas-gestao.funcionario');
+        $funcionarios = Funcionario::all();
+        return view('usuarios.telas-gestao.funcionario', compact('funcionarios'));
 
     }
 
-    public function adicionar()
+    public function adicionar(FuncionarioRequest $funcionarioRequest)
     {
+
+        if($funcionarioRequest)
+        {
+
+            $data = [
+            "nome" => $funcionarioRequest->nome,
+            "sobrenome"=> $funcionarioRequest->sobrenome,
+             "username" => $funcionarioRequest->username,
+             "email" => $funcionarioRequest->email,
+             "password" => $funcionarioRequest->password,
+             "cargo" => $funcionarioRequest->cargo
+        ];
+
+        $criarFuncionario = Funcionario::create($data);
+
+        if($criarFuncionario)
+        {
+             return redirect()->back()->with("message", "Funcionário registrado com sucesso");
+        }
+
+        else
+        {
+            return session()->flash('message', 'Erro Inesperado: Tente novamente!');
+        }
+
+        }
 
     }
 }
