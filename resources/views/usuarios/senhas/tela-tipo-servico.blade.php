eb86f6b660aa1b1edbcf7f6b5dde9b4e2a8b2e92
jcoder-dev <jorgeadao625@gmail.com>
2023-07-06 20:37:01 +0100

    alteracoes na tela de servico

--- resources/views/usuarios/senhas/tela-tipo-servico.blade.php
+++ resources/views/usuarios/senhas/tela-tipo-servico.blade.php
@@ -9,20 +9,19 @@
     <div class="col-md-10 col-sm-10 text-center m-auto">
         <div class="col-10 text-center m-auto">
             <ul class="list-group">
-                <li class="list-group-item list-group-item-action rounded mt-3 opcao">Solicitar Informações</li>
-                <li class="list-group-item list-group-item-action rounded mt-3 opcao">Solicitar com Notas</li>
-                <li class="list-group-item list-group-item-action rounded mt-3 opcao">Solicitar em Notas </li>
-                <li class="list-group-item list-group-item-action rounded mt-3 opcao">Solicitar Diploma</li>
-                <li class="list-group-item list-group-item-action rounded mt-3 opcao">Solicitar Carta de Recomendação p/ Estágio</li>
-                <li class="list-group-item list-group-item-action rounded mt-3 opcao">Inscrição para Preparatório</li>
-                <li class="list-group-item list-group-item-action rounded mt-3 opcao">Inscrição para Ingresso na Ucan</li>
+
+                @foreach ($servicos as $servico)
+                  <a href=""><li class="list-group-item list-group-item-action rounded mt-3 opcao">{{ $servico->nome }}</li></a>
+
+                @endforeach
+
             </ul>
          </div>
          <hr class="h-color mx-2">
         <div class="btn-group shadow-sm col-sm-12 p-2 bg-body rounded-circle" id="btn-custom">
             <button type="button" id="botaoRequisitarSenha" class="btn btn-outline-primary rounded-circle">
                 Requisitar Senha <br> <i class="fas fa-receipt"></i>
-            </button>
+
        </div>
     </div>
  </div>
