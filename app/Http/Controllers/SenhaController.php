b3adec4c9f337fe49cd64cf43a6a89a2b8a5bc1c
jcoder-dev <jorgeadao625@gmail.com>
2023-07-06 20:28:34 +0100

    add: Selecionar condicao e mostrar servicos

--- app/Http/Controllers/SenhaController.php
+++ app/Http/Controllers/SenhaController.php
@@ -2,13 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SenhaRequest;
+use App\Models\Servico;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SenhaController extends Controller
 {
     public function index()
     {
         return view('usuarios.senhas.dashboard');
+    }
+
+    public function selecionarServico(Request $request)
+    {
+
+        $estudante = DB::select('select * from estudantes where nmero_processo = ?', [$request->idEstudante]);
+
+        if($estudante)
+        {
+
+            $servicos = Servico::all();
+
+            return view('usuarios.senhas.tela-tipo-servico', compact('servicos'));
+        }
+
+
+
+    }
+
+    public function gerarSenha(Request $requestSenha)
+    {
+        if($requestSenha)
+        {
+
+        }
     }
 
 }
