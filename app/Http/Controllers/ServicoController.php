85d3f3bbd9627bc4978c368d98c1026ba2db1ee9
jcoder-dev <jorgeadao625@gmail.com>
2023-07-06 12:00:51 +0100

    add: Adicionar e deletar servico

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -2,13 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ServicoRequest;
+use App\Models\Servico;
 use Illuminate\Http\Request;
 
 class ServicoController extends Controller
 {
     public function index()
     {
-        return view('usuarios.telas-gestao.servico');
+
+        $servicos =  Servico::all();
+        return view('usuarios.telas-gestao.servico', compact('servicos'));
 
     }
+
+    public function adicionar(ServicoRequest $requestServico)
+    {
+        if($requestServico)
+            {
+                
+                $data = [
+                    "codigo" => $requestServico->codigo,
+                    "nome" => $requestServico->nome,
+                    "descricao" => $requestServico->descricao
+                ];
+
+                $servico = Servico::create($data);
+                if($servico)
+                    return redirect()->back()->with('message', "Servico registrado com sucesso!");
+
+            }
+    }
+
+    public function atualizar()
+    {
+
+    }
+
+    public function deletar($idServico)
+    {
+        $servico = Servico::find($idServico);
+        if($servico->delete())
+              return redirect()->back()->with('message', "Servico eliminado com sucesso!");
+
+
+    }
+
+
 }
